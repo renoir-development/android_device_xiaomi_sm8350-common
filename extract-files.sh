0dfc04fd39f3685cd017e65046d65aa51f301e52
Michael Bestas <mkbestas@lineageos.org>
2019-10-08 21:47:36 +0300

    sm8350-common: Remove android.hidl.base@1.0 shims
    * Patch the 2 remaining blobs that depend on it.
    * They already need libhidltransport.so so there's
      no real need for the shims anymore.
    
    Change-Id: I00f185fd4d1d8798893cf82abf9d64fcb68c3301

--- extract-files.sh
+++ extract-files.sh
@@ -63,8 +63,14 @@ function blob_fixup() {
         product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
             sed -i 's/version="2.0"/version="1.0"/g' "${2}"
             ;;
+        system_ext/lib64/libwfdnative.so)
+            "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
+            ;;
         vendor/etc/camera/pureShot_parameter.xml)
             sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
+            ;;
+        vendor/lib64/android.hardware.secure_element@1.0-impl.so)
+            "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
         vendor/etc/vintf/manifest/c2_manifest_vendor.xml)
             sed -ni '/ozoaudio/!p' "${2}"
