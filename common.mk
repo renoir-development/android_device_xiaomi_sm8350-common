95b52700bfea199b85b4d3cd8e27d005e650b360
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-12 01:00:17 +0200

    sm8350-common: Uprev bluetooth audio HIDL impl to 2.1
    * We get free LC3 support, isn't it poggers?
    
    Change-Id: I0f1713ee542102374183ef8088bcad62d3859c2f

--- common.mk
+++ common.mk
@@ -116,7 +116,7 @@ PRODUCT_PACKAGES += \
     libbthost_if
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     com.dsi.ant@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
