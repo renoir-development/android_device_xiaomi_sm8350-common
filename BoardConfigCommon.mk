dd642ea34e6b4979250deb60a8d9c0d964b9bc8a
Arian <arian.kulmer@web.de>
2021-10-26 18:35:16 +0200

    We are booted

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -114,6 +114,8 @@ BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load))
 BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.recovery))
