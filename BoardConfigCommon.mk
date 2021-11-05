99c967603f76b4b56c9b177f8e828a5cb9c5f531
Arian <arian.kulmer@web.de>
2021-11-05 16:44:55 +0100

    sm8350-common: Add power HAL extension and use xiaomi-touch to handle dt2w
    Change-Id: I5c65e0df255853eb2b3018a27ceb807c111bc4df

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -182,7 +182,7 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_BOARD_PLATFORM := lahaina
 
 # Power
-TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/touch/tp_dev/double_tap"
+TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
