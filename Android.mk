e5567f7b2365421ce82aac8beb0e4f31b86325d3
Nico Kümmel <nico.kuemmel@gmail.com>
2021-11-16 13:26:21 +0100

    sm8350-common: Add support for odin
    Change-Id: I7a657b9a8c5732860bb120472f14fe4c83fb6e22

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter haydn renoir venus,$(TARGET_DEVICE)),)
+ifneq ($(filter haydn odin renoir venus,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "haydn renoir venus"
+write_headers "haydn odin renoir venus"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
