00cc75d63eb29d010fe49e4a09aee00bbd3faec6
Arian <arian.kulmer@web.de>
2021-12-05 22:21:54 +0100

    Revert "[dnm] sm8350-common: Set selinux to permissive"
    This reverts commit 4a27494f19b0a964605bb082f7111caf1596607c.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,8 +109,6 @@ BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-
 # HIDL
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
