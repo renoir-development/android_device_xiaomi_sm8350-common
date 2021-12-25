3e786ede2d98adca8187a9f3335bb99436cc3ad5
Erfan Abdi <erfangplus@gmail.com>
2021-12-25 10:12:21 +0330

    sm8350-common: Build camera hal packages

--- common.mk
+++ common.mk
@@ -142,6 +142,13 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    libcamera2ndk_vendor \
+    libgui_vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
+
+PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_COPY_FILES += \
