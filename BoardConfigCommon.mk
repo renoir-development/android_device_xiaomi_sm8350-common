a20342f49339b96df5af42475abe27ea72d8f308
Arian <arian.kulmer@web.de>
2021-12-29 21:02:00 +0000

    sm8350-common: Add xiaomi_touch.ko to boot modules
    Change-Id: I73dbdd6a9b05b1a673e058cdbe3ce061b1c9756d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -116,7 +116,8 @@ BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 BOOT_KERNEL_MODULES := \
     hwid.ko \
     focaltech_touch.ko \
-    goodix_core.ko
+    goodix_core.ko \
+    xiaomi_touch.ko
 BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
 
 # HIDL
