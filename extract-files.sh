cd043f8a7fbb84436fdf8143f324b0a9556b1394
Arian <arian.kulmer@web.de>
2022-02-16 22:53:48 +0100

    sm8350-common: Add c2_manifest_vendor.xml
    Drop ozoaudio and dolby instances since we don't ship the blobs for them
    
    Change-Id: Ic56917e6b80aba7d9b123849bad9c42cfb9de872

--- extract-files.sh
+++ extract-files.sh
@@ -57,6 +57,10 @@ function blob_fixup() {
         vendor/etc/camera/pureShot_parameter.xml)
             sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
             ;;
+        vendor/etc/vintf/manifest/c2_manifest_vendor.xml)
+            sed -ni '/ozoaudio/!p' "${2}"
+            sed -ni '/dolby/!p' "${2}"
+            ;;
     esac
 }
 
