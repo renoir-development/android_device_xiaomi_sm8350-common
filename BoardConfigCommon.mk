c8b0769084d6f9fc2508bfbea37b02035d6d623e
Arian <arian.kulmer@web.de>
2022-02-18 01:12:28 +0100

    sm8350-common: Upate vendor security patch level from odin V12.5.12.0.RKMCNXM
    Change-Id: Idaa8ed9461ccfd5ff419fb996c037e30b5aac459

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -193,7 +193,7 @@ TARGET_USES_MKE2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-07-01
+VENDOR_SECURITY_PATCH := 2021-10-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
