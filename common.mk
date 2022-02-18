c981aae8d23c4ba15da72578a28bd50ea6426e7e
Arian <arian.kulmer@web.de>
2022-02-18 01:44:00 +0100

    sm8350-common: Add missing blobs
    Change-Id: Ie03636699c08f8e2d2cf9a549b469775e7ac247c

--- common.mk
+++ common.mk
@@ -117,7 +117,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    vendor.qti.hardware.fm@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
