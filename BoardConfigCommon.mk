cf97a426a13856e7ca907dabe25dd0315b5b43cc
Arian <arian.kulmer@web.de>
2022-02-25 19:13:09 +0100

    sm8350-common: Update display flags and define qtidisplay soong config
    The sm8350 display HAL moved to Android.bp and requires configs to be set
    via soong. Enable the default flags as per display-product.mk.
    
    Change-Id: If84afaf9cd87ad5b592e22bceebdd81b1ce5bce4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -66,8 +66,13 @@ TARGET_USES_QTI_CAMERA_DEVICE := true
 # Display
 TARGET_SCREEN_DENSITY := 440
 TARGET_USES_DISPLAY_RENDER_INTENTS := true
-TARGET_USES_GRALLOC4 := true
-TARGET_USES_HWC2 := true
+
+# Display soong
+SOONG_CONFIG_NAMESPACES += qtidisplay
+SOONG_CONFIG_qtidisplay := drmpp headless llvmsa gralloc4 default
+SOONG_CONFIG_qtidisplay_drmpp := true
+SOONG_CONFIG_qtidisplay_gralloc4 := true
+SOONG_CONFIG_qtidisplay_default := true
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
