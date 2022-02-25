b82d0b74597c18f80cc9245446e01d1df6fb34ec
ItsVixano <giovanniricca@protonmail.com>
2022-02-25 16:18:17 +0100

    [DNM] Dirty flashes go brrrr
    Change-Id: I40dc30bc764e7b76703eabf4d6caa8f7b6c95923

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -174,7 +174,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-10-01
+VENDOR_SECURITY_PATCH := 2021-12-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
