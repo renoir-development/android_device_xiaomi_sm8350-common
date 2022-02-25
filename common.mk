2b6ed57f26efa4fba5bcae6efeb0d64a73c22370
Arian <arian.kulmer@web.de>
2022-02-25 19:59:51 +0100

    sm8350-common: Drop unnecessary camera libs
    Change-Id: I40369b7b86065077b91e1051ff30352c065ee698

--- common.mk
+++ common.mk
@@ -141,8 +141,6 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    libcamera2ndk_vendor \
-    libgui_vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_PACKAGES += \
