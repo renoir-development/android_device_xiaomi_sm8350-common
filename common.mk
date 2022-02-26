c6f278816b3634214121622e174817522d77ce0e
Arian <arian.kulmer@web.de>
2022-02-26 13:08:13 +0100

    sm8350-common: Import init.qti.media.rc from odin V12.5.12.0.RKMCNXM
    Change-Id: Iab9a821bb7135b76205ce1a28ddc950ce852a42b

--- common.mk
+++ common.mk
@@ -229,6 +229,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/etc/init.qti.kernel.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qti.kernel.rc \
+    $(LOCAL_PATH)/rootdir/etc/init.qti.media.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qti.media.rc \
     $(LOCAL_PATH)/rootdir/etc/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.target.rc
 
 PRODUCT_COPY_FILES += \
