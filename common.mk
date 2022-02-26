5a3cfa0d8ef3902b47a5e06d030eeb2e72d3a199
Arian <arian.kulmer@web.de>
2022-02-26 01:08:10 +0100

    sm8350-common: Don't build powershare for all devices
    Change-Id: I4ad25058b73760f7ef6247acf9ef4725344e058c

--- common.mk
+++ common.mk
@@ -358,10 +358,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
-# Powershare
-PRODUCT_PACKAGES += \
-    vendor.lineage.powershare@1.0-service.xiaomi_sm8350
-
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
