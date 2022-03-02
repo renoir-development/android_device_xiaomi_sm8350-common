6db81f4eb2aab6ee293e46253d30dc586aa09c8a
Arian <arian.kulmer@web.de>
2022-03-02 17:36:52 +0100

    sm8350-common: Fix sorting of audio packages
    Change-Id: I281288fd80638a867b1e25bc5b208d425edc815e

--- common.mk
+++ common.mk
@@ -35,22 +35,22 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Audio
 PRODUCT_PACKAGES += \
-    libvolumelistener \
+    libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libqcompostprocbundle
+    libvolumelistener
 
 PRODUCT_PACKAGES += \
     audioadsprpcd \
     audio.a2dp.default \
-    audio.usb.default \
-    audio.r_submix.default
+    audio.r_submix.default \
+    audio.usb.default
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
-    android.hardware.soundtrigger@2.2-impl \
-    android.hardware.audio.service
+    android.hardware.audio.service \
+    android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_PACKAGES += \
     liba2dpoffload \
@@ -79,8 +79,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
-    frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
 # Component override
