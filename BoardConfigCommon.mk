9ed2d307d13141d94524c833cc32116f786a6481
Giovanni Ricca <giovanniricca@protonmail.com>
2022-05-19 14:02:16 +0200

    sm8350-common: Import libinit from xiaomi sm8250
    * https://github.com/LineageOS/android_device_xiaomi_sm8250-common/commits/lineage-18.1/libinit
    
    Co-Authored-By: Sebastiano Barezzi <barezzisebastiano@gmail.com>
    Co-Authored-By: Ramii Ahmed <ramy@ahmedramy.com>
    Co-Authored-By: luk1337 <priv.luk@gmail.com>
    Change-Id: I41c7f4c6efbe4a95adc79fd9a66578ba1a297745

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -84,6 +84,10 @@ DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/hidl/manifest_lineage.xml \
     $(COMMON_PATH)/hidl/manifest_xiaomi.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_lahaina
+TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_lahaina
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image
